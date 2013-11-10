df4b59351a9c4015818524a894e0e40adcc18ac4
Eliot Arntz <arntzel@gmail.com>
2013-11-10 19:28:05 +0100

    comments

--- View Objects in Code/CCViewController.m
+++ View Objects in Code/CCViewController.m
@@ -10,6 +10,9 @@
 
 @interface CCViewController ()
 
+@property (strong, nonatomic) UILabel *myLabel;
+@property (strong, nonatomic) UITextField *myTextField;
+
 @end
 
 @implementation CCViewController
@@ -18,6 +21,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /* Create UILabel in code. Set its' frame and add it to the view. */
+    self.myLabel = [[UILabel alloc] init];
+    self.myLabel.frame = CGRectMake(20, 60, 280, 22);
+    [self.view addSubview:self.myLabel];
+    
+    /* Setup some attributes for our label in this case the initial text and textColor. */
+    self.myLabel.textColor = [UIColor blueColor];
+    self.myLabel.text = @"placeholder";
+    
+    /* Create UITextField in code. Set its' frame and add it to the view. We also setup some initial attributes such as the placeholder text and background color. */
+    self.myTextField = [[UITextField alloc] init];
+    self.myTextField.frame = CGRectMake(20, 110, 280, 30);
+    self.myTextField.placeholder = @"Type text here";
+    self.myTextField.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.myTextField];
+    
+    /* Create UIButton in code of type UIButtonTypeRoundedRect which does some initial setup of our button. Set its' frame, title, target-action and add it to the view. We also setup some initial attributes such as the placeholder text and background color. */
+    UIButton *myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [myButton addTarget:self action:@selector(myButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [myButton setTitle:@"press me!" forState:UIControlStateNormal];
+    myButton.frame = CGRectMake(20, 160, 280, 30);
+    [self.view addSubview:myButton];
+}
+
+/* When the button is pressed update the label's text property to be equal to the textField's text property. Also hide the keyboard. */
+- (void)myButtonPressed:(UIButton *)sender
+{
+    self.myLabel.text = self.myTextField.text;
+    [self.myTextField resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning

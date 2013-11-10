3877af4601ba69aa029ca1c8f6a6b7079aaa9fd7
Eliot Arntz <arntzel@gmail.com>
2013-11-10 16:29:15 +0100

    Initial Commit

---
+++ View Objects in Code/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  View Objects in Code
+//
+//  Created by Eliot Arntz on 11/10/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
